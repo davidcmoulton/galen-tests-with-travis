f8f494c97466a290ade5bfc6bb8f564a1c8e91d8
David Moulton <d.moulton@elifesciences.org>
2015-11-23 22:50:20 +0000

    Update config

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,12 +1,6 @@
 #/bin/sh
-$dir=tests/galen/
-if [[ ! -e $dir ]]; then
-    mkdir -p $dir
-elif [[ ! -d $dir ]]; then
-    echo "$dir already exists but is not a directory" 1>&2
-    exit 1
-fi
-cd $dir;
+mkdir -p tests/galen
+cd tests/galen;
 curl https://github.com/galenframework/galen/releases/download/galen-2.1.3/galen-bin-2.1.3.zip -o galen-bin-2.1.3.zip
 unzip galen-bin-2.1.3.zip
 ln -s galen-2.1.3/galen galen
