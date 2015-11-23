d5b56c1a8f87e539adcff6f679e9bc8f2952ffd9
David Moulton <d.moulton@elifesciences.org>
2015-11-23 23:10:21 +0000

    Update config

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,4 +3,4 @@ mkdir -p tests/galen
 cd tests/galen;
 curl https://github.com/galenframework/galen/releases/download/galen-2.1.3/galen-bin-2.1.3.zip --create-dirs -o galen-bin-2.1.3.zip
 jar xvf galen-bin-2.1.3.zip
-ln -s galen-2.1.3/galen galen
+ln -s ./galen-2.1.3/galen ./galen
